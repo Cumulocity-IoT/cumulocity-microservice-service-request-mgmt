277b28e00744c19df0585eb624fd96ddab3ccb37
Alexander Pester <57684400+PestusAtSAG@users.noreply.github.com>
2023-05-23 14:38:51 +0200

    add id of service request event to alarm if exists (#5)
    * add id of service request event to alarm if exists
    
    * add service request active status counter to manged object
    
    ---------
    
    Co-authored-by: Pester <apes@softwareag.com>

---
+++ src/main/java/cumulocity/microservice/service/request/mgmt/service/c8y/ManagedObjectMapper.java
@@ -0,0 +1,84 @@
+package cumulocity.microservice.service.request.mgmt.service.c8y;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
+
+import cumulocity.microservice.service.request.mgmt.model.ServiceRequest;
+import lombok.extern.slf4j.Slf4j;
+
+public class ManagedObjectMapper {
+	
+	public static final String SR_ACTIVE_STATUS = "sr_ActiveStatus";
+	
+	private ManagedObjectRepresentation managedObjectRepresentation;
+	
+	public static ManagedObjectMapper map2(ManagedObjectRepresentation managedObjectRepresentation, ServiceRequest serviceRequest) {
+		if(managedObjectRepresentation == null || serviceRequest == null|| serviceRequest.getPriority() == null) {
+			return null;
+		}
+		
+		ManagedObjectMapper mapper = new ManagedObjectMapper(managedObjectRepresentation);		
+		return mapper;
+	}
+	
+	
+	
+	public ManagedObjectMapper(ManagedObjectRepresentation managedObjectRepresentation) {
+		this.managedObjectRepresentation = new ManagedObjectRepresentation();
+		this.managedObjectRepresentation.setId(managedObjectRepresentation.getId());
+		this.managedObjectRepresentation.set(managedObjectRepresentation.get(SR_ACTIVE_STATUS), SR_ACTIVE_STATUS);
+	}
+
+
+
+	public Map<String, Long> getServiceRequestPriorityCounterMap() {
+		return (Map<String, Long>) managedObjectRepresentation.get(SR_ACTIVE_STATUS);
+	}
+	
+	public void addServiceRequestPriorityCounter(String priorityName) {
+		if(priorityName == null) {
+			return;
+		}
+		Object object = managedObjectRepresentation.get(SR_ACTIVE_STATUS);
+		
+		if(object == null) {
+			Map<String, Long> priorityCounterMap = new HashMap();
+			priorityCounterMap.put(priorityName, 1L);
+			managedObjectRepresentation.set(priorityCounterMap, SR_ACTIVE_STATUS);
+			return;
+		}
+		
+		Map<String, Long> priorityCounterMap = (Map<String, Long>) object;
+		Long count = priorityCounterMap.get(priorityName);
+		priorityCounterMap.put(priorityName, count+1);
+		managedObjectRepresentation.set(priorityCounterMap, SR_ACTIVE_STATUS);
+	}
+	
+	public void removeServiceRequestPriorityCounter(String priorityName) {
+		if(priorityName == null) {
+			return;
+		}
+		Object object = managedObjectRepresentation.get(SR_ACTIVE_STATUS);
+		
+		if(object == null) {
+			Map<String, Long> priorityCounterMap = new HashMap();
+			priorityCounterMap.put(priorityName, 0L);
+			managedObjectRepresentation.set(priorityCounterMap, SR_ACTIVE_STATUS);
+			return;
+		}
+		
+		Map<String, Long> priorityCounterMap = (Map<String, Long>) object;
+		Long count = priorityCounterMap.get(priorityName);
+		priorityCounterMap.put(priorityName, count-1);
+		managedObjectRepresentation.set(priorityCounterMap, SR_ACTIVE_STATUS);
+	}
+
+
+
+	public ManagedObjectRepresentation getManagedObjectRepresentation() {
+		return managedObjectRepresentation;
+	}
+	
+}
--- src/main/java/cumulocity/microservice/service/request/mgmt/service/c8y/ServiceRequestServiceC8y.java
+++ src/main/java/cumulocity/microservice/service/request/mgmt/service/c8y/ServiceRequestServiceC8y.java
@@ -11,13 +11,17 @@ import org.springframework.stereotype.Service;
 
 import com.cumulocity.model.idtype.GId;
 import com.cumulocity.rest.representation.PageStatisticsRepresentation;
+import com.cumulocity.rest.representation.alarm.AlarmRepresentation;
 import com.cumulocity.rest.representation.event.EventRepresentation;
+import com.cumulocity.rest.representation.inventory.ManagedObjectRepresentation;
 import com.cumulocity.sdk.client.PagingParam;
 import com.cumulocity.sdk.client.QueryParam;
+import com.cumulocity.sdk.client.alarm.AlarmApi;
 import com.cumulocity.sdk.client.event.EventApi;
 import com.cumulocity.sdk.client.event.EventCollection;
 import com.cumulocity.sdk.client.event.EventFilter;
 import com.cumulocity.sdk.client.event.PagedEventCollectionRepresentation;
+import com.cumulocity.sdk.client.inventory.InventoryApi;
 
 import cumulocity.microservice.service.request.mgmt.controller.ServiceRequestPatchRqBody;
 import cumulocity.microservice.service.request.mgmt.controller.ServiceRequestPostRqBody;
@@ -32,12 +36,18 @@ public class ServiceRequestServiceC8y implements ServiceRequestService {
 	
 	private EventApi eventApi;
 	
+	private AlarmApi alarmApi;
+	
 	private EventAttachmentApi eventAttachmentApi;
+
+	private InventoryApi inventoryApi;
 	
 	@Autowired
-	public ServiceRequestServiceC8y(EventApi eventApi, EventAttachmentApi eventAttachmentApi) {
+	public ServiceRequestServiceC8y(EventApi eventApi, EventAttachmentApi eventAttachmentApi, AlarmApi alarmApi, InventoryApi inventoryApi) {
 		this.eventApi = eventApi;
 		this.eventAttachmentApi = eventAttachmentApi;
+		this.alarmApi = alarmApi;
+		this.inventoryApi = inventoryApi;
 	}
 
 	@Override
@@ -47,14 +57,41 @@ public class ServiceRequestServiceC8y implements ServiceRequestService {
 		eventMapper.setIsActive(Boolean.TRUE);
 		EventRepresentation createdEvent = eventApi.create(eventMapper.getEvent());
 		ServiceRequest newServiceRequest = ServiceRequestEventMapper.map2(createdEvent);
+		
+		//Update Alarm
+		AlarmMapper alarmMapper = AlarmMapper.map2(newServiceRequest);
+		if(alarmMapper != null) {
+			AlarmRepresentation alarmRepresentation = alarmMapper.getAlarm();
+			alarmApi.update(alarmRepresentation);
+		}
+		
+		//Update Managed Object
+		ManagedObjectRepresentation source = inventoryApi.get(GId.asGId(newServiceRequest.getSource().getId()));
+		ManagedObjectMapper moMapper = ManagedObjectMapper.map2(source, newServiceRequest);
+		moMapper.addServiceRequestPriorityCounter(newServiceRequest.getPriority().getName());
+		inventoryApi.update(moMapper.getManagedObjectRepresentation());
+		
 		return newServiceRequest;
 	}
 
 	@Override
 	public ServiceRequest updateServiceRequest(String id, ServiceRequestPatchRqBody serviceRequest) {
+		ServiceRequest originalServiceRequest = getServiceRequestById(id);
+		
 		ServiceRequestEventMapper eventMapper = ServiceRequestEventMapper.map2(id, serviceRequest);
 		EventRepresentation updatedEvent = eventApi.update(eventMapper.getEvent());
-		return eventMapper.map2(updatedEvent);
+		
+		ServiceRequest updatedServiceRequest = eventMapper.map2(updatedEvent);
+		
+		//Update Managed Object
+		if(Boolean.TRUE.equals(originalServiceRequest.getIsActive()) && Boolean.FALSE.equals(updatedServiceRequest.getIsActive())) {
+			ManagedObjectRepresentation source = inventoryApi.get(GId.asGId(updatedServiceRequest.getSource().getId()));
+			ManagedObjectMapper moMapper = ManagedObjectMapper.map2(source, updatedServiceRequest);
+			moMapper.removeServiceRequestPriorityCounter(updatedServiceRequest.getPriority().getName());
+			inventoryApi.update(moMapper.getManagedObjectRepresentation());			
+		}
+		
+		return updatedServiceRequest;
 	}
 	
 	@Override
