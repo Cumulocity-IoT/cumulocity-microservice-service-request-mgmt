d69296537ff8998a390f5a3276f7c3232559841e
Pester <apes@softwareag.com>
2023-08-12 22:06:04 +0200

    empty tests added

---
+++ src/test/java/cumulocity/microservice/service/request/mgmt/service/c8y/ServiceRequestCommentServiceC8yTest.java
@@ -0,0 +1,49 @@
+package cumulocity.microservice.service.request.mgmt.service.c8y;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ServiceRequestCommentServiceC8yTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testCreateComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCommentListByFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCommentById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateComment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUploadAttachment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDownloadAttachment() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/cumulocity/microservice/service/request/mgmt/service/c8y/ServiceRequestServiceC8yTest.java
@@ -0,0 +1,69 @@
+package cumulocity.microservice.service.request.mgmt.service.c8y;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ServiceRequestServiceC8yTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void testCreateServiceRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateServiceRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetServiceRequestById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllServiceRequestByFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetActiveServiceRequestByFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCompleteActiveServiceRequestByFilter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteServiceRequest() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUploadAttachment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDownloadAttachment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateServiceRequestStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateServiceRequestActive() {
+		fail("Not yet implemented");
+	}
+
+}
