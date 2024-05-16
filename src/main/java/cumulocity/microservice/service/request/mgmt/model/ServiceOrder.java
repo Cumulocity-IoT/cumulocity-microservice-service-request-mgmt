31ef3ce519d06369bd08f8fbfddb749088a5d4d8
Pester <apes@softwareag.com>
2024-05-16 11:39:24 +0200

    order entity ignore properties if null

--- src/main/java/cumulocity/microservice/service/request/mgmt/model/ServiceOrder.java
+++ src/main/java/cumulocity/microservice/service/request/mgmt/model/ServiceOrder.java
@@ -1,12 +1,11 @@
 package cumulocity.microservice.service.request.mgmt.model;
 
 import org.springframework.validation.annotation.Validated;
+import org.svenson.JSONProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-@Data
 @RequiredArgsConstructor
 @Schema(description = "Service Order object at Cumulocity IoT Platform.")
 @Validated
@@ -20,4 +19,32 @@ public class ServiceOrder {
 
     @Schema(required = false, description = "Status of order", example = "Pending")
     private String status;
+
+    @JSONProperty(ignoreIfNull = true)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JSONProperty(ignoreIfNull = true)
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    @JSONProperty(ignoreIfNull = true)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
