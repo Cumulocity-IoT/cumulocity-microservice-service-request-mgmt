3c91ce6880ac9004523b035ecfba7c739b3e5289
Pester <apes@softwareag.com>
2024-06-10 16:00:14 +0200

    http headers added

--- src/main/java/cumulocity/microservice/service/request/mgmt/service/c8y/DebugEventApi.java
+++ src/main/java/cumulocity/microservice/service/request/mgmt/service/c8y/DebugEventApi.java
@@ -1,6 +1,13 @@
 package cumulocity.microservice.service.request.mgmt.service.c8y;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class DebugEventApi {
-    
+
     	private ContextService<MicroserviceCredentials> contextService;
 	
 	    private final CumulocityClientProperties clientProperties;
@@ -27,9 +34,17 @@ public class DebugEventApi {
 
         public void test() {
             log.info("Test");
+
+            HttpHeaders headers = new HttpHeaders();
+		    headers.set("Authorization", contextService.getContext().toCumulocityCredentials().getAuthenticationString());
+		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
             String serverUrl = clientProperties.getBaseURL() + "/event/events?fragmentValue=NEW&pageSize=2000&fragmentType=sr_SyncStatus&type=c8y_ServiceRequest";
 		    RestTemplate restTemplate = new RestTemplate();
-            String response = restTemplate.getForObject(serverUrl, String.class);
-            log.info("Response: {}", response);
+            
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+            ResponseEntity<String> response = restTemplate.exchange(serverUrl, HttpMethod.GET, entity, String.class);
+
+            log.info("Response: {}", response.getBody());
         }
 }
