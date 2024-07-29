a2d1dadd8ff4d10c16df4be4a360205901e7173f
Alexander Pester <57684400+PestusAtSAG@users.noreply.github.com>
2024-07-29 12:32:29 +0200

    Improve Update response time (#76)
    change less important functions to async in order to response faster after service request update

--- src/main/java/cumulocity/microservice/service/request/mgmt/config/MicroserviceConfig.java
+++ src/main/java/cumulocity/microservice/service/request/mgmt/config/MicroserviceConfig.java
@@ -1,22 +1,46 @@
 package cumulocity.microservice.service.request.mgmt.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
+@EnableAsync
 public class MicroserviceConfig {
+
 	@Bean
 	@Primary
 	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-	    ObjectMapper objectMapper = builder.build();
-	    objectMapper.registerModule(new JodaModule());
-	    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-	    return objectMapper;
+		ObjectMapper objectMapper = builder.build();
+		objectMapper.registerModule(new JodaModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		return objectMapper;
+	}
+
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(20);
+		executor.setQueueCapacity(500);
+		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		executor.setThreadNamePrefix("AsyncExecutor-");
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setAwaitTerminationSeconds(60);
+		executor.initialize();
+		return executor;
 	}
 }
