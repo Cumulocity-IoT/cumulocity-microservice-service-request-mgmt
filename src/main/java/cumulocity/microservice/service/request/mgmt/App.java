d188d130a5b5736714562f9987e971ea2c6483c7
apes <alexander.pester@cumulocity.com>
2025-04-24 11:47:43 +0200

    validation job added

--- src/main/java/cumulocity/microservice/service/request/mgmt/App.java
+++ src/main/java/cumulocity/microservice/service/request/mgmt/App.java
@@ -1,10 +1,12 @@
 package cumulocity.microservice.service.request.mgmt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.cumulocity.microservice.autoconfigure.MicroserviceApplication;
 
 @MicroserviceApplication
+@EnableScheduling
 public class App {
     public static void main (String[] args) {
         SpringApplication.run(App.class, args);
